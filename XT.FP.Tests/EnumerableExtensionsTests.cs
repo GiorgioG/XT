8dc6f2924e28f551bc7bd867666a277cae155a5b
GiorgioG <giorgio@giorgiogalante.com>
2022-08-08 16:34:34 -0400

    Cleanup

--- XT.FP.Tests/EnumerableExtensionsTests.cs
+++ XT.FP.Tests/EnumerableExtensionsTests.cs
@@ -1,30 +1,26 @@
 using XT.Core;
 
-namespace XT.FP.Tests
-{
-    public class EnumerableExtensionsTests
-    {
-        [Fact]
-        public void Returns_New_List_With_Null_Arg()
-        {
-            IEnumerable<string> nullEnumerable = null;
-            var result = nullEnumerable.Map(s => s.ToUpper());
-            Assert.NotNull(result);
-            Assert.Empty(result);
-        } 
-        
-        [Fact]
-        public void Applies_Transform_On_Non_Empty_Arg()
-        {
-            var list = new List<string>() { "bob", "Bob", "BOB"};
-            var result = list.Map(s => s.ToUpper());
-            foreach (var value in result)
-            {
-                Assert.True(value.IsAllUpper());
-            }
-            
-        }
+namespace XT.FP.Tests;
 
-      
+public class EnumerableExtensionsTests
+{
+    [Fact]
+    public void Map_Returns_New_List_With_Null_Arg()
+    {
+        IEnumerable<string> nullEnumerable = null;
+        var result = nullEnumerable.Map(s => s.ToUpper());
+        Assert.NotNull(result);
+        Assert.Empty(result);
+    }
+
+    [Fact]
+    public void Map_Applies_Transform_On_Non_Empty_Arg()
+    {
+        var list = new List<string>() { "bob", "Bob", "BOB" };
+        var result = list.Map(s => s.ToUpper());
+        foreach (var value in result)
+        {
+            Assert.True(value.IsAllUpper());
+        }
     }
 }
